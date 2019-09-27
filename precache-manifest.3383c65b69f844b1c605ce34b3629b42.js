08c16afa78fdb545fcf96e67542869307945d58a
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-27 13:43:42 +0300

    Updates

---
+++ precache-manifest.3383c65b69f844b1c605ce34b3629b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2a460ff872f5389ccb893476263b0d",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "1392c527e3869ecf9806",
+    "url": "/react-carousel/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "38348a2bba5ea48441d4",
+    "url": "/react-carousel/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1392c527e3869ecf9806",
+    "url": "/react-carousel/static/js/2.5995aa7d.chunk.js"
+  },
+  {
+    "revision": "38348a2bba5ea48441d4",
+    "url": "/react-carousel/static/js/main.669aa2b3.chunk.js"
+  },
+  {
+    "revision": "a30389308560f2580fbc",
+    "url": "/react-carousel/static/js/runtime-main.1244b284.js"
+  }
+]);
